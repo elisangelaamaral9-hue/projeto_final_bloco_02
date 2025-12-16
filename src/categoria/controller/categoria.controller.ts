17b6c59b89fdade465b49cd7a2ffc700fb870424
Elisangela Amaral <elisangela.amaral9728@gmail.com>
2025-12-16 13:43:43 -0300

    Add HttpCode

--- src/categoria/controller/categoria.controller.ts
+++ src/categoria/controller/categoria.controller.ts
@@ -17,24 +17,28 @@ export class CategoriaController {
 
   // LISTAR TODOS   
   @Get()
+  @HttpCode(HttpStatus.OK)
   findAll() {
     return this.categoriaService.findAll();
   }
 
   // BUSCAR POR ID
   @Get("/:id")
+  @HttpCode(HttpStatus.OK)
   findById(@Param("id", ParseIntPipe) id: number) {
     return this.categoriaService.findById(id);
   }
 
   // BUSCAR POR NOME
   @Get("/nome/:nome")
+  @HttpCode(HttpStatus.OK)
   findByNome(@Param("nome") nome: string) {
     return this.categoriaService.findByNome(nome);
   }
 
   // ATUALIZAR CATEGORIA POR ID
   @Put("/:id")
+  @HttpCode(HttpStatus.OK)
   update(
     @Param("id", ParseIntPipe) id: number,
     @Body() categoria: Categoria
