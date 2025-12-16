8532c4be394b0def648e81f5dac3ddb99f0878e4
Elisangela Amaral <elisangela.amaral9728@gmail.com>
2025-12-16 10:51:22 -0300

    Conexão MySQL

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+process.env.TZ = '-03:00'
+
+app.useGlobalPipes (new ValidationPipe());
+
+// Configurando o CORS
+app.enableCors({
+    origin: 'https://meusite.com', // Substitua pelo domínio permitido
+    methods: 'GET,POST,PUT,DELETE', // Métodos permitidos
+    allowedHeaders: 'Content-Type, Authorization', // Cabeçalhos permitidos
+  });
+
+await app.listen(process.env.PORT ?? 4000);
 }
+
 bootstrap();
